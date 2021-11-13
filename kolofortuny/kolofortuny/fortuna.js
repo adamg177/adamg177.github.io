ef6430d6d203aa675719b314f66ad739367d571b
adamg177 <adamgrzesiak2@tlen.pl>
2021-11-13 21:56:10 +0100

    bez pomocy osoby doswiadczonej w css masakra

--- kolofortuny/kolofortuny/fortuna.js
+++ kolofortuny/kolofortuny/fortuna.js
@@ -1,34 +1,31 @@
-
-var game = {
-  zdobyte : 0,
-  zycia : 3,
-}
+var game = {zdobyte: 0,zycia: 3,};
 var wybor = 0;
 var znacznik = [];
 
-document.getElementById("Sprawdz litere".addEventListener("click",Sprawdz_Litery));
-document.getElementById("Zamknij".addEventListener("click",author));
 
-function start(){
-  document.getElementById("start").addEventListener("click",plansza);
-  document.getElementById("autor").addEventListener("click",author)
-  document.getElementById("wrap").style.display="none";
-}
+document.getElementById("Sprawdź litere").addEventListener("click", Sprawdz_Litery);
+document.getElementById("zamknij").addEventListener("click", author);
+
 function author() {
   if (document.getElementById("authorinfo").style.display === "none")
     document.getElementById("authorinfo").style.display = "";
   else
     document.getElementById("authorinfo").style.display = "none";
 }
+function start() {
 
-function plansza(){
-  document.getElementById("autor").style.display="none";
-  document.getElementById("start").style.display="none";
-  document.getElementById("wrap").style.display="";
-  document.getElementById("blad").innerHTML = "";
+  document.getElementById("start").addEventListener("click",plansza);
+  document.getElementById("autor").addEventListener("click",author);
+  document.getElementById("wrap").style.display = "none";
+}
+function plansza() {
   document.getElementById("panstwa").innerHTML = "";
+  document.getElementById("blad").innerHTML = ""
+  document.getElementById("wrap").style.display = "";
+  document.getElementById("start").style.display="none";
+  document.getElementById("autor").style.display="none";
 
-  wybor = getRandomInit(0, data.length)
+  wybor = random(0, data.length)
   console.log(data[wybor]['country']);
 
   var newDiv = document.createElement("div");
@@ -47,9 +44,10 @@ function plansza(){
   zmianazyc(0);
 }
 
-function sprawdzlit(){
+
+function Sprawdz_Litery() {
   var litera = document.getElementById("wpisz_litere").value;
-  var litery = document.getElementById("litera");
+  var litery = document.getElementsByClassName("litera");
   var zgd = false;
 
   for (var i = 0; i < litery.length; i += 1) {
@@ -58,46 +56,23 @@ function sprawdzlit(){
       zgd = true;
     }
   }
+
   znacznik.push(litera);
-}
   
 
-function wyg(){
- /*  for (var i = 0; i < data[0]['country'].length; i += 1) {
-    console.log(data[0]['country'][i]);  
-  }*/
 
-  var litery = document.getElementByClassName("litera")
-    for (let i = 0; i < litery.length; i += 1) {
-    if (litery[i].classList.contains("ukryte")) {
-      return false;
-    }
-  }
-  return true;
+
+function zmianazyc(change) {
+  game.zycia += change;
+  document.getElementById("zycie").innerHTML = ('Życia: ' + game.zycia);
 }
 
-function addElement(mydiv){
-
-  var newDiv = document.createElement("span");
-  newDiv.innerHTML = litera.toUpperCase();
-  node.append(newDiv);
-  newDiv.classList.add(style);
-  newDiv.classList.add("litera")
-}
-
-function random(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function zmianapkt(change){
+function zmpkt(change) {
   game.zdobyte += change;
-  document.getElementById("punkty").innerHTML=('Odgadnięte państwa:  ' + game.zdobyte);
-
+  document.getElementById("punkty").innerHTML = ('Odgadnięte państwa: ' + game.zdobyte);
 }
 
-if (zgd) {
+  if (zgd) {
     if (wyg()) {
       alert("Odgadłeś państwo :)");
       plansza();
@@ -115,12 +90,32 @@ if (zgd) {
       znacznik= [];
     }
   }
-  
-
-function zmianazyc(change){
-  game.zycia += change;
-  document.getElementById("zycie").innerHTML=('Życia:  ' + game.zycia);
 }
 
-start()
+function wyg() {
+  var litery = document.getElementsByClassName("litera");
+  for (let i = 0; i < litery.length; i += 1) {
+    if (litery[i].classList.contains("ukryte")) {
+      return false;
+    }
+  }
+  return true;
+}
 
+function addElement(node, litera, style) {
+
+  var newDiv = document.createElement("span");
+  newDiv.innerHTML = litera.toUpperCase();
+  node.append(newDiv);
+
+  newDiv.classList.add("litera");
+  newDiv.classList.add(style);
+}
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+start();
